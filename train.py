bbb97c1f8d23a0540f2af92d3ebf839a0f9c4690
Jiri Kula <jiri.kula@nullspaces.com>
2023-08-03 17:49:37 +0200

    edgetpu_compiler compatible

--- train.py
+++ train.py
@@ -126,6 +126,7 @@ model.save_weights("model_good_full")
 
 # model.input.set_shape((1, 256, 256, 3))
 model.compute_output_shape((1, 320, 320, 3))
+model.load_weights('fit/epoch_1000')
 
 def representative_dataset():
     for _ in range(100):
@@ -145,10 +146,12 @@ converter.representative_dataset = representative_dataset
 # For full integer quantization, though supported types defaults to int8 only, we explicitly declare it for clarity.
 # converter.target_spec.supported_types = [tf.int8]
 # These set the input and output tensors to uint8 (added in r2.3)
-converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-converter.inference_input_type = tf.uint8  # or tf.uint8
-converter.inference_output_type = tf.uint8  # or tf.uint8
 converter.experimental_new_converter = False
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8,
+                                       tf.lite.OpsSet.SELECT_TF_OPS]
+converter.inference_input_type = tf.int8  # or tf.uint8
+converter.inference_output_type = tf.int8  # or tf.uint8
+# converter.experimental_new_converter = False
 tflite_model = converter.convert()
 
 # Save the model.

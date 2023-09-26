6d9f80b0384298c6d1dd60570517579f5a62d079
Jiri Kula <jiri.kula@nullspaces.com>
2023-09-26 14:09:39 +0200

    good_areas, good_aspects

--- anchor_histogram.py
+++ anchor_histogram.py
@@ -1,10 +1,12 @@
 # %%
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 from sklearn.cluster import KMeans
 
-meta_train = "/mnt/c/Edwards/annotation/RV12/merge_3_class.csv"
+
+meta_train = "/mnt/c/Edwards/annotation/RV12/merge-all.csv"
 
 column_names = [
     "PURPOSE",
@@ -39,8 +41,8 @@ hs = y2 - y1
 aspects = ws / hs
 
 a = np.array(aspects)
-w = np.array(ws)
-h = np.array(hs)
+w = np.array(ws) * 320
+h = np.array(hs) * 320
 
 # %% K-means
 kmeans = KMeans(init="random", n_clusters=5, n_init=10, max_iter=300, random_state=42)
@@ -48,8 +50,34 @@ kmeans = KMeans(init="random", n_clusters=5, n_init=10, max_iter=300, random_sta
 # fit = kmeans.fit(np.vstack((ws, hs)).transpose())
 fit = kmeans.fit(w.reshape(-1, 1))
 
-areas = 320**2 * h * w
-sorted(np.sqrt(kmeans.fit(areas.reshape(-1, 1)).cluster_centers_))
+areas = h * w
+good_areas = np.array(
+    sorted(np.sqrt(kmeans.fit(areas.reshape(-1, 1)).cluster_centers_))
+).reshape(1, -1)
+print("good_areas: ", good_areas)
 
 # aspects = w / h
 # %%
+freq, edges = np.histogram(aspects)
+center = (edges[1:] + edges[:-1]) / 2
+plt.bar(center, freq)
+
+
+# %%
+def stats(x):
+    print("min: {:f}\nmax: {:f}".format(min(x), max(x)))
+
+
+print("width\n---------------------------")
+stats(w)
+
+print("height\n---------------------------")
+stats(h)
+
+# %% Aspect ratios
+kmeans = KMeans(init="random", n_clusters=3, n_init=10, max_iter=300, random_state=42)
+
+fit = kmeans.fit(a.reshape(-1, 1))
+
+aspects = np.array(sorted(fit.cluster_centers_)).reshape((1, -1))
+print("aspects: ", aspects)

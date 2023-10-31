cf95f5f1057a4322f224578d6608a01aa991e077
Jiri Kula <jiri.kula@nullspaces.com>
2023-10-31 08:46:06 +0100

    display single sample

--- explore_dataset.py
+++ explore_dataset.py
@@ -4,11 +4,11 @@ from tfrecord_decode import decode_raw
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
-
+import time
 from mpl_toolkits.axes_grid1 import ImageGrid
 
 #%%
-example_path = "zaznamy_z_vyroby.tfrecord"
+example_path = "/home/jiri/winpart/Edwards/zaznamy_z_vyroby.tfrecord"
 # train_data = tf.data.TFRecordDataset(example_path).map(decode_fn)
 
 im = []
@@ -25,7 +25,7 @@ boxes = []
 #             anchor_encoding = label[idx, :4]
 
 i = 0
-for item in tf.data.TFRecordDataset([example_path]).map(decode_raw):
+for item in tf.data.TFRecordDataset([example_path]).map(decode_raw).skip(800*64).take(10):
     # image
     image = tf.io.decode_png(item["image_raw"])
     image = tf.image.resize(image, [320, 320])
@@ -46,6 +46,18 @@ for item in tf.data.TFRecordDataset([example_path]).map(decode_raw):
     assert(tf.reduce_all(tf.greater(xmaxes, xmins)))
     assert(tf.reduce_all(tf.greater(ymaxes, ymins)))
 
+    # angles
+    r11 = tf.sparse.to_dense(item["r11s"], default_value=0.)
+    r21 = tf.sparse.to_dense(item["r21s"], default_value=0.)
+    r31 = tf.sparse.to_dense(item["r31s"], default_value=0.)
+    r12 = tf.sparse.to_dense(item["r12s"], default_value=0.)
+    r22 = tf.sparse.to_dense(item["r22s"], default_value=0.)
+    r32 = tf.sparse.to_dense(item["r32s"], default_value=0.)
+
+    if not (tf.reduce_all(tf.equal([r11, r21, r31, r12, r22, r32], 0.0))):
+        tf.print(item["image_path"])
+        tf.print([r11, r21, r31, r12, r22, r32])
+
     i += 1
     print('\r>> You have finished %d' % i, end="")
     sys.stdout.flush()
@@ -53,21 +65,36 @@ for item in tf.data.TFRecordDataset([example_path]).map(decode_raw):
     # example.ParseFromString(batch.numpy())
     # print(example)
 
+    plt.imshow(tf.cast(image, tf.uint8))
+
+    for i in range(0, len(xmins)):
+        x1 = xmins[i] * 320
+        y1 = ymins[i] * 320
+        x2 = xmaxes[i] * 320
+        y2 = ymaxes[i] * 320
+        patch = plt.Rectangle(
+            [x1, y1], x2-x1, y2-y1, fill=False, edgecolor=[0, 1, 0], linewidth=1
+        )
+        plt.gca().add_patch(patch)
+
+
+    plt.show()
+
     
-fig = plt.figure(figsize=(4., 4.))
-grid = ImageGrid(fig, 111,  # similar to subplot(111)
-                 nrows_ncols=(2, 2),  # creates 2x2 grid of axes
-                 axes_pad=0.1,  # pad between axes in inch.
-                 )
+# fig = plt.figure(figsize=(4., 4.))
+# grid = ImageGrid(fig, 111,  # similar to subplot(111)
+#                  nrows_ncols=(2, 2),  # creates 2x2 grid of axes
+#                  axes_pad=0.1,  # pad between axes in inch.
+#                  )
 
-for ax, image in zip(grid, im):
-    # Iterating over the grid returns the Axes.
-    ax.imshow(image)
-    patch = plt.Rectangle(
-        [80, 100], 50, 90, fill=False, edgecolor=[0, 1, 0], linewidth=1
-    )
-    ax.add_patch(patch)
+# for ax, image in zip(grid, im):
+#     # Iterating over the grid returns the Axes.
+#     ax.imshow(image)
+#     patch = plt.Rectangle(
+#         [80, 100], 50, 90, fill=False, edgecolor=[0, 1, 0], linewidth=1
+#     )
+#     ax.add_patch(patch)
 
 
-plt.show()
+# plt.show()
 

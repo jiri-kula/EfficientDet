9859c7dcc0617a0fb3f6352f58ba02584d5d0ab6
Jiri Kula <jiri.kula@nullspaces.com>
2024-09-26 18:56:28 +0200

    working with tfds

--- anchor_histogram_tfrecord.py
+++ anchor_histogram_tfrecord.py
@@ -8,95 +8,45 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn.cluster import KMeans
-
-# from tfrecord_files import train_list
-from tfrecord_decode import decode_raw, raw2label
-
-# laod list of tfrecord files
-with open("list.txt") as file:
-    train_list = [line.rstrip() for line in file]
-
-ds = tf.data.TFRecordDataset(train_list).map(decode_raw)
-
-# %%
-ws = []
-hs = []
-
-for item in ds:
-    image, gt_boxes, gt_classes, gt_angles = raw2label(item)
-    for box in gt_boxes:
-        ws.append(box[2].numpy())
-        hs.append(box[3].numpy())
-
-w = np.array(ws)
-h = np.array(hs)
-
-a = w / h
-
-
-def stats(x):
-    print("min: {:f}\nmax: {:f}".format(min(x), max(x)))
-
-
-stats(w)
-stats(a)
-
-# here w and h are widths and heights of bounding boxes found in the dataset, need to compute aspect rations, scales and areas to setup anchors.py
-
-
-# %% K-means
-kmeans = KMeans(init="random", n_clusters=5, n_init=10, max_iter=300, random_state=42)
-
-# fit = kmeans.fit(np.vstack((ws, hs)).transpose())
-fit = kmeans.fit(w.reshape(-1, 1))
-
-areas = h * w
-good_areas = np.array(
-    sorted(np.sqrt(kmeans.fit(areas.reshape(-1, 1)).cluster_centers_))
-).reshape(1, -1)
-print("good_areas: ", np.round(good_areas))
-
-
-# aspects = w / h
-# %%
-freq, edges = np.histogram(a)
-center = (edges[1:] + edges[:-1]) / 2
-plt.bar(center, freq)
+import tensorflow as tf
+import tensorflow_datasets as tfds
 
 
 # %%
-def stats(x):
-    print("min: {:f}\nmax: {:f}".format(min(x), max(x)))
+def analyze_bounding_boxes(dataset_name, split="train"):
+    # Load the dataset
+    ds = tfds.load(dataset_name, split=split, shuffle_files=False)
+
+    # Initialize min and max bounding box dimensions
+    min_bbox = [float("inf"), float("inf")]
+    max_bbox = [float("-inf"), float("-inf")]
+
+    # Iterate over the dataset
+    for example in ds:
+        objects = example["objects"]
+        bboxes = objects["bbox"]
+
+        for bbox in bboxes:
+            ymin, xmin, ymax, xmax = bbox.numpy()
+            width = xmax - xmin
+            height = ymax - ymin
+
+            # Update min and max dimensions
+            if width < min_bbox[0]:
+                min_bbox[0] = width
+            if height < min_bbox[1]:
+                min_bbox[1] = height
+            if width > max_bbox[0]:
+                max_bbox[0] = width
+            if height > max_bbox[1]:
+                max_bbox[1] = height
+
+    # Output the results
+    print(f"Minimum bounding box dimensions: {min_bbox}")
+    print(f"Maximum bounding box dimensions: {max_bbox}")
 
 
-print("width\n---------------------------")
-stats(w)
+# Example usage
+analyze_bounding_boxes("kk_dataset:1.0.1")
 
-print("height\n---------------------------")
-stats(h)
-
-# %% Aspect ratios
-kmeans = KMeans(init="random", n_clusters=3, n_init=10, max_iter=300, random_state=42)
-
-fit = kmeans.fit(a.reshape(-1, 1))
-
-aspects = np.array(sorted(fit.cluster_centers_)).reshape((1, -1))
-print("aspects: ", aspects)
-
-# %%
-
-
-def split(areas, N):
-    area_min = min(areas)
-    area_max = max(areas)
-    h = (area_max - area_min) / N
-    area_center = np.linspace(area_min + h / 2, area_max - h / 2, N)
-
-    return area_center
-
-
-print("areas:", np.round(np.sqrt(split(w * h, 5))))
-print("aspects:", split(w / h, 3))
-# %%
 # %%
